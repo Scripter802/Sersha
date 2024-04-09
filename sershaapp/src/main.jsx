704faf095d8669acb605bc9a0be96c3e9e0afd10
RR <rajko.fed@gmail.com>
2024-04-09 08:45:45 +0200

    first commit

---
+++ sershaapp/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import './index.css'
+import HomePage from './pages/HomePage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);

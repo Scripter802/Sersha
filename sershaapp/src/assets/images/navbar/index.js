aff129133a8e33f9cff0907fefe0284e2a0cc3f9
RR <rajko.fed@gmail.com>
2024-04-10 10:43:13 +0200

    navbar

--- sershaapp/src/assets/images/navbar/index.js
+++ sershaapp/src/assets/images/navbar/index.js
@@ -5,5 +5,19 @@ import map from "./map.png";
 import messages from "./messages.png";
 import rectangle from "./rectangle.png";
 import search from "./search.png";
+import coin from "./coin.png";
+import avatar from "./userpick.png";
+import level from "./level.png";
 
-export { logo, game, home, map, messages, rectangle, search };
+export {
+  logo,
+  game,
+  home,
+  map,
+  messages,
+  rectangle,
+  search,
+  coin,
+  level,
+  avatar,
+};
--- sershaapp/src/components/Header.jsx
+++ sershaapp/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import './header.css'
-import { game, home, logo, map, messages, rectangle, search } from './../assets/images/navbar/index.js'
+import { game, home, logo, map, messages, rectangle, search, coin, level, avatar } from './../assets/images/navbar/index.js'
 
 const Header = () => {
   return (
@@ -12,7 +12,26 @@ const Header = () => {
             <div className='messages'><img src={messages} alt="messages" /></div>
             <div className='search'><img src={search} alt="search" /></div>
         </div>
-        <div></div>
+        <div className='rightWrapper'>
+          <div>
+            <div className='coinWrapper'>
+              <img src={coin} alt="coin" />
+              <p>3,483</p>  
+            </div>
+          </div>
+          <div className='profileWrapper'>
+            
+              <p>Camelia</p>
+              <p>Level 2</p>
+            
+          </div>
+
+          <div className='avatarWrapper'>
+            <img src={level} alt="level" className='avatar' />
+            <img src={avatar} alt="avatar" className='avatar' />
+          </div>
+          
+        </div>
     </div>
   )
 }

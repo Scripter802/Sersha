d358b4644eff688acbfeafd3ff15c0c570a568d4
RR <rajko.fed@gmail.com>
2024-05-28 15:36:35 +0200

    add

---
+++ sershaapp/src/assets/images/slideshow/easyBundle/Clues/PNG_s/index.js
@@ -0,0 +1,18 @@
+import digitalCompassPng from './Level 1. Digital Compass.png'
+import secretAgentBadePng from './Level 2. Secret Agent Bade.png'
+import fragmentedChipPng from './Level 9. Fragmented Chip.png'
+import virtualKeyPng from './Level 16. Virtual Key.png'
+import glowingEmojiPng from './Level 23. Glowing Emoji.png'
+import harmonyCrystalPng from './Level 30. Harmony Crystal.png'
+import halfAPhotographPng from './Level 39.Half A Photograph.png'
+
+
+export default {
+  digitalCompassPng,
+  secretAgentBadePng,
+  fragmentedChipPng,
+  virtualKeyPng,
+  glowingEmojiPng,
+  harmonyCrystalPng,
+  halfAPhotographPng,
+}
---
+++ sershaapp/src/assets/images/slideshow/easyBundle/Narrative/index.js
@@ -0,0 +1,68 @@
+import gif2 from './2.gif'
+import gif3 from './3.gif'
+import gif4 from './4.gif'
+import gif5 from './5.gif'
+import gif6 from './6.gif'
+import gif7 from './7.gif'
+import gif8 from './8.gif'
+import gif9 from './9.gif'
+import gif10 from './10.gif'
+import gif11 from './11.gif'
+import gif12 from './12.gif'
+import gif13 from './13.gif'
+import gif14 from './14.gif'
+import gif15 from './15.gif'
+import gif16 from './16.gif'
+import gif17 from './17.gif'
+import gif18 from './18.gif'
+import gif19 from './19.gif'
+import gif20 from './20.gif'
+import gif21 from './21.gif'
+import gif22 from './22.gif'
+import gif23 from './23.gif'
+import gif24 from './24.gif'
+import gif25 from './25.gif'
+import gif26 from './26.gif'
+import gif28 from './28.gif'
+import gif29 from './29.gif'
+import gif30 from './30.gif'
+import gif31 from './31.gif'
+import gif32 from './32.gif'
+import gif33 from './33.gif'
+import gif34 from './34.gif'
+
+
+export default {
+  gif2,
+  gif3,
+  gif4,
+  gif5,
+  gif6,
+  gif7,
+  gif8,
+  gif9,
+  gif10,
+  gif11,
+  gif12,
+  gif13,
+  gif14,
+  gif15,
+  gif16,
+  gif17,
+  gif18,
+  gif19,
+  gif20,
+  gif21,
+  gif22,
+  gif23,
+  gif24,
+  gif25,
+  gif26,
+  gif28,
+  gif29,
+  gif30,
+  gif31,
+  gif32,
+  gif33,
+  gif34,
+}

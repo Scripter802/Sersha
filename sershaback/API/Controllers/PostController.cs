c1981b27754116f6e963a2e5686aee961652478d
Dimitrije-DotWebDev <dimitrije.krstic@outlook.com>
2024-05-28 13:23:35 +0200

    [FromForm]

--- sershaback/API/Controllers/PostController.cs
+++ sershaback/API/Controllers/PostController.cs
@@ -39,7 +39,7 @@ namespace API.Controllers
         }
 
         [HttpPut("{id}")]
-        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        public async Task<ActionResult<Unit>> Edit([FromForm] Guid id, Edit.Command command)
         {
             command.Id = id;
             return await Mediator.Send(command);

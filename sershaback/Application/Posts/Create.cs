dadfd0508036949f569a431e2ca7d04dcc3f584e
Dimitrije-DotWebDev <dimitrije.krstic@profitoptics.com>
2024-06-23 01:13:24 +0200

    minor image path fix

--- sershaback/Application/Posts/Create.cs
+++ sershaback/Application/Posts/Create.cs
@@ -66,7 +66,7 @@ namespace Application.Posts
                     
                 };
                 
-                String path = Directory.GetCurrentDirectory() + "\\wwwroot\\Images\\postImages\\" + request.Stage;
+                String path = Directory.GetCurrentDirectory() + "\\wwwroot\\wwwroot\\Images\\postImages\\" + request.Stage;
                 if(request.Image != null){
                     FileInfo fi = new FileInfo(request.Image.FileName);
                     string fileName = request.Title + fi.Extension;

837bcc6b46d96ebd9210844703d2a01a92ef6451
RR <rajko.fed@gmail.com>
2024-06-22 18:58:57 +0200

    allow-origin

--- sershaapp/src/components/signInUp/RegisterForm.jsx
+++ sershaapp/src/components/signInUp/RegisterForm.jsx
@@ -61,6 +61,7 @@ const RegisterForm = () => {
         headers: {
           'Content-Type': 'application/json',
           'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
         },
         body: JSON.stringify(userData),
       });

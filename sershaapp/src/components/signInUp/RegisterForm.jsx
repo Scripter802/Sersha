cf3f9866185483f43c44d34c66519a6c4fb29b47
RR <rajko.fed@gmail.com>
2024-06-22 21:06:59 +0200

    register login

--- sershaapp/src/components/signInUp/RegisterForm.jsx
+++ sershaapp/src/components/signInUp/RegisterForm.jsx
@@ -46,6 +46,13 @@ const RegisterForm = () => {
   const handleRegister = async (e) => {
     e.preventDefault();
 
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    if (!passwordRegex.test(registerPassword)) {
+      setErrorMessage('Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character.');
+      return;
+    }
+
     if (registerPassword !== registerRePassword) {
       setErrorMessage('Passwords do not match');
       return;

0ff463707266ab1b381832b62f145e64f12ea2dd
Dimitrije-DotWebDev <dimitrije.krstic@profitoptics.com>
2024-06-24 15:25:16 +0200

    Get single message returns whole conversation

--- sershaback/Application/Chat/GetRandomChatMessage.cs
+++ sershaback/Application/Chat/GetRandomChatMessage.cs
@@ -42,8 +42,27 @@ namespace Application.Chats
                 {
                     throw new Exception("Message not found");
                 }
-
+                await LoadNextMessages(message.Responses, cancellationToken);    
+                   
                 return message;
+            }
+
+            private async Task LoadNextMessages(List<UserResponse> responses, CancellationToken cancellationToken)
+            {
+                foreach (var response in responses)
+                {
+                    if (response.NextMessageId.HasValue)
+                    {
+                        response.NextMessage = await _context.ChatMessages
+                            .Include(m => m.Responses)
+                            .FirstOrDefaultAsync(m => m.Id == response.NextMessageId.Value, cancellationToken);
+
+                        if (response.NextMessage != null)
+                        {
+                            await LoadNextMessages(response.NextMessage.Responses, cancellationToken);
+                        }
+                    }
+                }
             }
         }
     }
